3d3860c7614740a9ece6d194648c3212cdefbbe3
Alex Colson <alex@ajcolson.com>
2025-11-16 18:14:46 -0500

    Various Small Changes
    Some of the site pages are a bit more fleshed out, but still not 100% complete.
    
    The top nav is now dynamically generated.
    
    Cards on the main page are dynamically generated.
    
    Dark/Light themes can be swapped (currently only from the console).

--- global.js
+++ global.js
@@ -1,6 +1,122 @@
-const DeckBuilder = {
-  MakeCard:(CardInfo=null)=>{
-    if (CardInfo === null)
-      return;
+//
+// Local Storage Shorthand methods
+//
+let LS = {
+  prefix:"__sdev153__",
+  g:(name)=>{
+    return localStorage.getItem(`${LS.prefix}${name}`)
+  },
+  s:(name,val)=>{
+    localStorage.setItem(`${LS.prefix}${name}`, val)
+  },
+  d:(name,val)=>{
+    if (localStorage.getItem(`${LS.prefix}${name}`) == null){
+      localStorage.setItem(`${LS.prefix}${name}`, val)
+    }
+  }
+}
+
+
+//
+// Theme Management
+//
+function ToggleTheme(){
+  if (LS.g("theme") == null){
+    LS.d("theme","dark")
+  }
+
+  let currentTheme = LS.g("theme")
+  let newTheme = (currentTheme=="dark")?"light":"dark"
+
+  document.querySelector("body").dataset.bsTheme = newTheme
+  LS.s("theme",newTheme)
+}
+
+//
+// Instead of having to manually edit the navbar on every page, let's just do this dynamically on every page from one master list.
+//
+let TopBarNavItems = [
+  {
+    "type": "link",
+    "text": "Home",
+    "href": "/"
+  },
+  {
+    "type": "link",
+    "text": "About",
+    "href": "/about/"
+  },
+  {
+    "type": "dropdown",
+    "text": "Pages",
+    "href": "#",
+    "sub_nav":[
+      {
+        "text": "Hello World!",
+        "href":"/pages/hello"
+      },
+      {
+        "text": "Ipsum",
+        "href":"/pages/Ipsum"
+      },
+      {
+        "text": "JavaScript Under Pressure",
+        "href":"/pages/javascript-under-pressue/"
+      }
+    ]
+  }
+]
+function BuildTopNavBar(){
+  let NavBar = document.querySelector("#MainNavbar .navbar-nav")
+
+  for (let Item of TopBarNavItems){
+    if (Item.type == "dropdown"){
+      
+      let DropDownNavItem = document.createElement("li")
+      DropDownNavItem.classList.add("nav-item","dropdown")
+      
+      let DropDownItemLink = document.createElement("a")
+      DropDownItemLink.classList.add("nav-link", "dropdown-toggle")
+      DropDownItemLink.href = Item.href
+      DropDownItemLink.role = "button"
+      DropDownItemLink.dataset.bsToggle = "dropdown"
+      DropDownItemLink.ariaExpanded = "false"
+      DropDownItemLink.innerHTML = Item.text
+
+      DropDownNavItem.append(DropDownItemLink)
+
+      let NavDropdownMenu = document.createElement("ul")
+      NavDropdownMenu.classList.add("dropdown-menu")
+      
+      for (let DropDownItem of Item.sub_nav){
+        let DropDownNavItem = document.createElement("li")
+        let DropDownItemLink = document.createElement("a")
+        DropDownItemLink.classList.add("dropdown-item")
+        DropDownItemLink.href = DropDownItem.href
+        DropDownItemLink.innerHTML = DropDownItem.text
+
+        DropDownNavItem.append(DropDownItemLink)
+        NavDropdownMenu.append(DropDownNavItem)
+      }
+      DropDownNavItem.append(NavDropdownMenu)
+      
+      NavBar.append(DropDownNavItem)
+    } else {
+      let NavItem = document.createElement("li")
+      NavItem.classList.add("nav-item")
+
+      let NavItemLink = document.createElement("a")
+      NavItemLink.classList.add("nav-link")
+      NavItemLink.href = Item.href
+      NavItemLink.innerHTML = Item.text
+
+      if (window.location.pathname == Item.href){
+        NavItemLink.classList.add("active")
+        NavItem.ariaCurrent="page"
+      }
+
+      NavItem.append(NavItemLink)
+      NavBar.append(NavItem)
+    } 
   }
 }
--- index.js
+++ index.js
@@ -1,3 +1,79 @@
+const CardsInMyHand = [
+  /* TEMPLATE
+  {
+    "title":"",
+    "text":"",
+    "action_text":"",
+    "icon":"",
+    "target":""
+  }
+  */
+  {
+    "title": "Hello World!",
+    "text": "Come say hello to the world...",
+    "action_text": "Read the article",
+    "icon":"",
+    "target":"./pages/hello"
+  }, {
+    "title": "Ipsum",
+    "text": "Let's do more than just Lorem.",
+    "action_text": "Read the article",
+    "icon":"",
+    "target":"./pages/Ipsum"
+ }, {
+    "title": "JavaScript Under Pressure",
+    "text": "How good is your JavaScript skills?",
+    "action_text": "Read the article",
+    "icon":"",
+    "target":"./pages/javascript-under-pressue"
+ }
+]
+
+const DeckBuilder = {
+  MakeCard:(Card=null)=>{
+    if (Card === null)
+      return;
+      /* TEMPLATE
+      {
+        "title":"",
+        "text":"",
+        "action_text":"",
+        "icon":"",
+        "target":""
+      }
+      */
+
+    let card = document.createElement("div")
+    card.classList.add("card","m-3","front-page-card", "d-flex")
+
+    let card_body = document.createElement("div")
+    card_body.classList.add("card-body")
+
+    let card_title = document.createElement("h5")
+    card_title.classList.add("card-title")
+    card_title.innerHTML = Card.title
+    card_body.append(card_title)
+
+    let card_text = document.createElement("p")
+    card_text.classList.add("card-text")
+    card_text.innerHTML = Card.text
+    card_body.append(card_text)
+
+    let card_action_btn = document.createElement("a")
+    card_action_btn.classList.add("btn","btn-primary")
+    card_action_btn.href = Card.target
+    card_action_btn.innerHTML = Card.action_text
+    card_body.append(card_action_btn)
+
+    card.append(card_body);
+    document.querySelector("#deck").append(card)
+
+  }
+}
+
+
 document.addEventListener("DOMContentLoaded", (e)=>{
-  console.log(`Why are you looking at this? There is nothing to see here ... yet ...`)
+  for (let card of CardsInMyHand)
+    DeckBuilder.MakeCard(card)
+  BuildTopNavBar()
 })
