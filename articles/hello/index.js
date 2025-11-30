02618a12d98c0921d45a7fd9aee31762014e34e4
Alex Colson <alex@ajcolson.com>
2025-11-30 17:39:53 -0500

    Add Clear button and extra text to hello page

--- articles/hello/index.js
+++ articles/hello/index.js
@@ -9,7 +9,12 @@ document.querySelector("#msgForm").addEventListener("submit",(e)=>{
   let text = document.querySelector("#msgText").value
 
   document.querySelector("#modal_msgTarget").innerHTML = (target != "") ? target : "World"
-  document.querySelector("#modal_msgText").innerHTML = (text != "") ? text : `Thank you for visiting this page. Click the "Close" button below to dismiss this message.`
+  document.querySelector("#modal_msgText").innerHTML = (text != "") ? text : `This is a demo of a program/website displaying a hello world message. Click the "Close" button below or the X button at the top right of this window to dismiss this message.`
 
   e.preventDefault()
+})
+
+document.querySelector("#clear-form-btn").addEventListener("click",(e)=>{
+  document.querySelector("#msgTarget").value = ""
+  document.querySelector("#msgText").value = ""
 })
